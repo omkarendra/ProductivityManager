d2c2b6c450c7c4dcdfb5f58dfefb2f13b1d13787
Omkarendra Tiwari <Omkarendra.Tiwari@bennett.edu.in>
2026-08-27 11:05:04 +0530

    refactor: prepare for Flutter promo-engine

--- src/promo/todo/category.py
+++
@@ -1,5 +0,0 @@
-class Category:
-
-    def __init__(self, name, category_id=None):
-        self.id = category_id
-        self.name = name
--- src/promo/todo/task.py
+++
@@ -1,9 +0,0 @@
-
-class Task:
-    def __init__(self, title, description=None,category=None, category_id=None, status=None, task_id=None):
-        self.id = task_id
-        self.title = title
-        self.description = description
-        self.category = category
-        self.category_id = category_id
-        self.status = status if status is not None else "pending"  # Default status is "pending"
