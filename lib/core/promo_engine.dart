383b7a0989810ebcfba0f3ca88d9ced12ac23286
Omkarendra Tiwari <Omkarendra.Tiwari@bennett.edu.in>
2026-09-01 15:02:55 +0530

    Make plugins list unmodifiable when passing

--- lib/core/promo_engine.dart
+++ lib/core/promo_engine.dart
@@ -2,45 +2,46 @@ import 'plugin.dart';
 import 'plugin_manager.dart';
 
 class PromoEngine {
-  PromoEngine({
-    required List<Plugin> plugins,
-  }) : _plugins = plugins;
+    PromoEngine({
+        required List<Plugin> plugins,
+    }) : _plugins = List.unmodifiable(plugins);
 
-  final List<Plugin> _plugins;
-  late final PluginManager _pluginManager;
+    final List<Plugin> _plugins;
+    late final PluginManager _pluginManager;
 
-  bool _started = false;
+    bool _started = false;
 
-  bool get isStarted => _started;
+    bool get isStarted => _started;
 
-List<Plugin> get plugins {
-  if (!_started) {
-    throw StateError('PromoEngine has not been started');
-  }
-
-  return _pluginManager.plugins;
-}
-  Future<void> start() async {
-    if (_started) {
-      throw StateError('PromoEngine is already started');
-    }
-
-    _pluginManager = PluginManager();
-
-    for (final plugin in _plugins) {
-      await _pluginManager.register(plugin);
-    }
-
-    _started = true;
-  }
-
-  Future<void> stop() async {
+    List<Plugin> get plugins {
     if (!_started) {
-      return;
+        throw StateError('PromoEngine has not been started');
     }
 
-    await _pluginManager.dispose();
-    _started = false;
-  }
+    // return _pluginManager.plugins;
+    return List.unmodifiable(_pluginManager.plugins);
+    }
+    Future<void> start() async {
+        if (_started) {
+            throw StateError('PromoEngine is already started');
+        }
+
+        _pluginManager = PluginManager();
+
+        for (final plugin in _plugins) {
+            await _pluginManager.register(plugin);
+        }
+
+        _started = true;
+    }
+
+    Future<void> stop() async {
+        if (!_started) {
+            return;
+        }
+
+        await _pluginManager.dispose();
+        _started = false;
+}
 
 }
